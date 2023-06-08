275652e1c7a28879483db9b80d4ee24c8ec76fc1
Nils Engelbach <nils.engelbach@trumpf.com>
2023-06-08 14:09:34 +0000

    Add Method with Workaround

--- Source/TransactionScopeExample/ExampleService.cs
+++ Source/TransactionScopeExample/ExampleService.cs
@@ -40,6 +40,35 @@ public class ExampleService
     }
 
     // This is working - but not same transaction
+    public async Task ExampleMethodWithWorkaround()
+    {
+        using (var transaction = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+        {
+            // Make changes to entities
+            var entity = _dbContext.ExampleEntities.Add(new ExampleEntity() { Name = new Random().Next(1, 100).ToString() });
+            await _dbContext.SaveChangesAsync();
+
+            // Add job for entity
+            var scheduler = await _schedulerFactory.GetScheduler();
+            var job = JobBuilder.Create<ExampleJob>()
+                .WithIdentity("exampleJob")
+                .UsingJobData("entity-name", entity.Entity.Name)
+                .Build();
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity("exampleTrigger")
+                .StartNow()
+                .Build();
+            
+            using (var suppressedTransactionScope = new TransactionScope(TransactionScopeOption.Suppress, TransactionScopeAsyncFlowOption.Enabled))
+            {
+                await scheduler.ScheduleJob(job, trigger);
+            }
+
+            transaction.Complete();
+        }
+    }
+
+    // This is working - but not same transaction
     public async Task ExampleMethodWithoutScope()
     {
         // Make changes to entities
